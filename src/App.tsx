d2512c7622d0403fcbe3fe5a5c8f1d614921b353
kevin julio <kevinjulio93@outlook.com>
2023-07-23 11:43:10 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.scss'
+import Button from '@mui/material/Button';
+
+
+function App() {
+  
+  return (
+    <div>
+      <h1>
+        Qualty app
+        <Button variant='contained'>Hello world</Button>
+      </h1>
+    </div>  
+  )
+}
+
+export default App
