d5620d2b2c58ea2bd3079c0da2fdf7272a05a7ed
kevin julio <kevinjulio93@outlook.com>
2023-07-30 15:10:39 -0500

    change login layout

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,10 +1,11 @@
 import './login.scss'
-import { Button, Card, TextField } from '@mui/material';
+import { Button, TextField } from '@mui/material';
 import { useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom';
 import logo from '../../assets/logo.png';
+import loginImg from '../../assets/login-img.jpg';
 import { useState } from 'react';
-import {  } from "module";
+import { } from "module";
 // import { FecthRequestModel } from '../../models/request.model';
 import { setUser } from '../../features/auth/authSlice';
 import { ROUTES } from '../../constants/routes';
@@ -14,14 +15,14 @@ function Login() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   // const loginRequest = FecthRequestModel.getInstance();
-  const [credentials, setCredentials] = useState({email:'', password: ''});
+  const [credentials, setCredentials] = useState({ email: '', password: '' });
 
 
-  const formHanlder = (target: 'email'| 'password', e:any) => {
-    setCredentials({...credentials, [target]:e.target.value});
+  const formHanlder = (target: 'email' | 'password', e: any) => {
+    setCredentials({ ...credentials, [target]: e.target.value });
   }
 
-  const login = async (e:any)=> {
+  const login = async (e: any) => {
     e.preventDefault()
     // const response = await loginRequest.post('/users/login', credentials, true);
     dispatch(setUser({
@@ -34,34 +35,39 @@ function Login() {
   }
 
   return (
-    <div className='login-container'>
-      <Card className='login-container__card'>
-        <img src={logo} alt="Logo" style={{ width: '50px' }} />
-        
-        <h3>Bienvenido de vuelta a Qualty Senior</h3>
+    <div className='login-view'>
+      <div className='login-view__image-container'>
+        <img src={loginImg} alt='Logo' />
+      </div>
 
-        <form className="login-container__card__form-container"  onSubmit={login}>
-          <TextField
-            className='login-container__form-container__field'
-            id="user"
-            name='email'
-            label="User"
-            variant="outlined"
-            onChange={(e)=>formHanlder('email', e)}
+      <div className='login-view__login-form'>
+        <div className='login-view__login-form__card'>
+          <img className='login-view__login-form__card__logo' src={logo} alt="Logo" />
+          <span className='login-view__login-form__card__title'>Bienvenido a Qualty</span>
+          <form className="login-view__login-form__card__form-container" onSubmit={login}>
+            <TextField
+              className='login-view__login-form__form-container__input'
+              id="user"
+              name='email'
+              onChange={(e) => formHanlder('email', e)}
+              placeholder='Email'
             />
 
-          <TextField
-            className='login-container__form-container__field'
-            id="pass"
-            name='password'
-            label="Password"
-            variant="outlined"
-            onChange={(e)=>formHanlder('password', e)}
+
+            <TextField
+              className='login-view__login-form__form-container__input'
+              id="pass"
+              name='password'
+              placeholder='Contraseña'
+              type='password'
+              onChange={(e) => formHanlder('password', e)}
             />
 
-          <Button variant="contained" type='submit'>Ingresar</Button>
-        </form>
-      </Card>
+            <Button variant="contained" type='submit'>Ingresar</Button>
+          </form>
+        </div>
+
+      </div>
     </div>
   )
 }
