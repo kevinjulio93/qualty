70f56cbcbd51b1b28121f7648d99d02701255df2
kevin julio <kevinjulio93@outlook.com>
2023-08-04 00:34:03 -0500

    create logout in topbar component

--- src/components/topBar/topBar.tsx
+++ src/components/topBar/topBar.tsx
@@ -3,15 +3,31 @@ import LogoutIcon from '@mui/icons-material/Logout';
 import logo from '../../assets/logo.png'
 import avatar from '../../assets/avatar.jpg'
 import './topBar.scss'
+import { useDispatch } from 'react-redux';
+import { setLogout } from '../../features/auth/authSlice';
+import { AppUser } from '../../models/user.model';
+import { useNavigate } from 'react-router-dom';
+import { ROUTES } from '../../constants/routes';
 
 function TopBar() {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate()
+
+    const logoutFunction = () => {
+        localStorage.removeItem('user');
+        const user = new AppUser();
+        dispatch(setLogout({...user}))
+        navigate(ROUTES.DASHBOARD)
+      }
+
     return (
         <>
             <Paper className='topbar-conintaer' sx={{ height: '60px', width: '100%', position: 'fixed' }}>
                 <div className='topbar-conintaer__logo'> <img src={logo} alt="" /></div>
                 <div className='topbar-conintaer__profile'>
                     <Avatar alt="Remy Sharp" src={avatar} />
-                    <LogoutIcon></LogoutIcon>
+                    <LogoutIcon onClick={logoutFunction} />
                 </div>
             </Paper>
         </>
