fe98773b5e900a40b97a621dffa5a8e3976b703f
Guillermo <glrc2001@gmail.com>
2023-09-14 21:56:29 -0500

    Enhacement - Implementar Toast notification component y Validar login cuando se agrega un usuario o contraseña invalida

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -12,6 +12,7 @@ import { userLogin } from "../../services/login.service";
 import { AppUser } from "../../models/user.model";
 import { getReferences } from "../../services/references.service";
 import { setReference } from "../../features/referencesSlice";
+import Toast from "../../components/toast/toast";
 
 function Login() {
   const navigate = useNavigate();
@@ -19,12 +20,15 @@ function Login() {
   const [credentials, setCredentials] = useState({ email: "", password: "" });
   const [loading, setLoading] = useState(false);
 
+  const [toastErrorLogin, setToastErrorLogin] = useState(false);
+  const [toastEmailRequired, setToastEmailRequired] = useState(false);
+  const [toastPasswordRequired, setToastPasswordRequired] = useState(false);
+
   const formHanlder = (target: "email" | "password", e: any) => {
     const value =
       target === "email" ? (e.target.value as string).trim() : e.target.value;
     setCredentials({ ...credentials, [target]: value });
   };
-
 
   const getAllReferences = async () => {
     try {
@@ -34,13 +38,22 @@ function Login() {
         dispatch(setReference({ ...references }));
       }
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   };
 
   const login = async (e: any) => {
     e.preventDefault();
     setLoading(true);
+    const { email, password } = credentials;
+    if (!email.trim()) {
+      setToastEmailRequired(true);
+      return;
+    }
+    if (!password.trim()) {
+      setToastPasswordRequired(true);
+      return;
+    }
     try {
       const response = await userLogin(credentials);
       if (response.status === 200) {
@@ -54,13 +67,13 @@ function Login() {
       }
     } catch (error) {
       setLoading(false);
+      setToastErrorLogin(true);
     }
   };
 
   const validCredentials = (): boolean => {
     return credentials.email === "" || credentials.password === "";
   };
-
 
   return (
     <div className="login-view">
@@ -108,6 +121,24 @@ function Login() {
           </form>
         </div>
       </div>
+      <Toast
+        open={toastErrorLogin}
+        handleClose={() => setToastErrorLogin(false)}
+        message="Ocurrio un error al iniciar sesion"
+        severity="error"
+      />
+      <Toast
+        open={toastEmailRequired}
+        handleClose={() => setToastEmailRequired(false)}
+        message="Debe llenar el campo email"
+        severity="error"
+      />
+      <Toast
+        open={toastPasswordRequired}
+        handleClose={() => setToastPasswordRequired(false)}
+        message="Debe llenar el campo contraseña"
+        severity="error"
+      />
     </div>
   );
 }
