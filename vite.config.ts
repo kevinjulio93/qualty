2461e56d4e6e7bb62d9d979b34b94057fa97e620
Hernando Clareth <hernando.ariza@ormuco.com>
2023-09-19 17:50:47 -0500

    fixing DP lib import

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,21 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import sass from 'sass'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    manifest: true,
+    rollupOptions: {
+      input: {
+        'index.js': 'src/modules/WebSdk/index.js',
+      },
+    },
+  },
+  define: {
+    'process.env': {
+      NODE_ENV: process.env.NODE_ENV,
+    },
+  },
   css: {
     preprocessorOptions: {
       scss: {
