ca1f163229397658bad42c5c3edf93bcf2705b17
Guillermo <glrc2001@gmail.com>
2023-10-20 15:57:11 -0500

    Agregar select para seleccionar el departamento, el municipio y la comuna

--- src/components/select.tsx
+++ src/components/select.tsx
@@ -1,42 +1,58 @@
-import { FormControl, InputLabel, MenuItem, Select } from '@mui/material'
+import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { useEffect, useState } from "react";
 
 interface IProp {
-    label?: string,
-    value?: string,
-    options?: Array<any>,
-    keyLabel?: string,
-    keyValue?: string,
-    handleValue?: any,
-    targetKey?: string
-    selectValue?:string;
-
+  label?: string;
+  value?: string;
+  options?: Array<any>;
+  keyLabel?: string;
+  keyValue?: string;
+  handleValue?: any;
+  targetKey?: string;
+  selectValue?: string;
 }
 
 function SelectDropdown(props: IProp) {
-    const value = props.keyValue ? props.keyValue : 'value';
-    return (
-        <FormControl style={{ width: '100%' }}>
-            <InputLabel id={props.value}>{props.label}</InputLabel>
-            <Select
-                labelId={props.value}
-                id={props.label}
-                label={props.label}
-                onChange={(e)=> props.handleValue(props.targetKey, e)}
-                value={props.selectValue || ''}
-            >
-                {props.options?.length && props.options.map((option: any, index: number) => {
-                    return (
-                        <MenuItem
-                            key={index}
-                            defaultValue={''}
-                            value={option[`${value}`]}>
-                            {props.keyLabel ? option[props.keyLabel] : option.label}
-                        </MenuItem>
-                    )
-                })}
-            </Select>
-        </FormControl>
-    )
+  const [newValue, setNewValue] = useState<any>(undefined);
+  const selectValue = (e: any) => {
+    const selectedItem = props.options?.find(
+      (item) => item[props.keyValue as string] === e.target.value
+    );
+    const selectedValue = {
+      id: selectedItem[props.keyValue as string],
+      label: selectedItem[props.keyLabel as string],
+    };
+    props.handleValue(props.targetKey, selectedValue);
+    setNewValue(selectedValue.id);
+  };
+
+  return (
+    <FormControl style={{ width: "100%" }}>
+      <InputLabel id={props.value}>{props.label}</InputLabel>
+      <Select
+        labelId={props.value}
+        id={props.label}
+        label={props.label}
+        onChange={(e) => selectValue(e)}
+        value={newValue ?? props.selectValue ?? ""}
+      >
+        {props.options?.length &&
+          props.options.map((option: any, index: number) => {
+            return (
+              <MenuItem
+                key={index}
+                defaultValue={""}
+                value={option[props.keyValue as string]}
+              >
+                {props.keyLabel
+                  ? option[props.keyLabel as string]
+                  : option.label}
+              </MenuItem>
+            );
+          })}
+      </Select>
+    </FormControl>
+  );
 }
 
-export default SelectDropdown
+export default SelectDropdown;
