8b107feebe6eced43a42174450da072ffb0b344c
kevin julio <kevinjulio93@outlook.com>
2023-11-07 13:29:34 -0500

    modifying date column in lists

--- src/layout/dashboard/dashboardLayout.tsx
+++ src/layout/dashboard/dashboardLayout.tsx
@@ -1,54 +1,63 @@
-import { useEffect, useState } from 'react';
-import PersonIcon from '@mui/icons-material/Person';
-import AssistWalkerIcon from '@mui/icons-material/AssistWalker';
-import RemoveRedEyeIcon from '@mui/icons-material/RemoveRedEye';
-import LibraryBooksIcon from '@mui/icons-material/LibraryBooks';
-import InsertEmoticonIcon from '@mui/icons-material/InsertEmoticon';
-import DraftsIcon from '@mui/icons-material/Drafts';
-import './dashboard-layout.scss';
-import avatar from '../../assets/avatar01.png'
-import { Outlet, useNavigate } from 'react-router-dom';
-import { ROUTES } from '../../constants/routes';
-import { setLogout } from '../../features/auth/authSlice';
-import { useDispatch, useSelector } from 'react-redux';
-import { AppUser } from '../../models/user.model';
-import SideMenu from '../../components/sideMenu/sideMenu';
-import { RootState } from '../../app/store';
+import { useEffect, useState } from "react";
+import PersonIcon from "@mui/icons-material/Person";
+import AssistWalkerIcon from "@mui/icons-material/AssistWalker";
+import RemoveRedEyeIcon from "@mui/icons-material/RemoveRedEye";
+import LibraryBooksIcon from "@mui/icons-material/LibraryBooks";
+import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+import DraftsIcon from "@mui/icons-material/Drafts";
+import "./dashboard-layout.scss";
+import avatar from "../../assets/avatar01.png";
+import { Outlet, useNavigate } from "react-router-dom";
+import { ROUTES } from "../../constants/routes";
+import { setLogout } from "../../features/auth/authSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { AppUser } from "../../models/user.model";
+import SideMenu from "../../components/sideMenu/sideMenu";
+import { RootState } from "../../app/store";
 
 const DashboardLayout = () => {
   const [open, setOpen] = useState(false);
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-  const loggedUser:AppUser = useSelector((state: RootState) => state.auth.user);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const loggedUser: AppUser = useSelector(
+    (state: RootState) => state.auth.user
+  );
   const iconList = {
     person: <PersonIcon />,
     book: <LibraryBooksIcon />,
     draft: <DraftsIcon />,
     assist: <AssistWalkerIcon />,
     eye: <RemoveRedEyeIcon />,
-    smile: <InsertEmoticonIcon />
+    smile: <InsertEmoticonIcon />,
   };
 
-
   const logoutFunction = () => {
-    localStorage.removeItem('user');
+    localStorage.removeItem("user");
     const user = new AppUser();
-    dispatch(setLogout({ ...user }))
-    navigate(ROUTES.DASHBOARD)
+    dispatch(setLogout({ ...user }));
+    navigate(ROUTES.DASHBOARD);
+  };
+
+  const toogleMenu = () => {    
+    setMenuOpen(!menuOpen);
   }
 
   return (
     <div className="dashboard-layout">
-
+      <button className="show-menu-icon" onClick={toogleMenu}>show menu</button>
       <div className="dashboard-layout__page-view">
-        <div className="dashboard-layout__page-view__menu">
-          <a href='#'>Qualty</a>
-          <div className='dashboard-layout__page-view__menu__logo'> <img src={avatar} alt="" /></div>
-          <div className='dashboard-layout__page-view__menu__info-user'>
+        <div className={`dashboard-layout__page-view__menu ${menuOpen ? 'show-menu' : ''}`}>
+          <a href="#">Qualty</a>
+          <div className="dashboard-layout__page-view__menu__logo">
+            {" "}
+            <img src={avatar} alt="" />
+          </div>
+          <div className="dashboard-layout__page-view__menu__info-user">
             <h5>{loggedUser?.name}</h5>
             <p>{loggedUser?.role?.role}</p>
           </div>
-          <SideMenu logoutF={logoutFunction}/>
+          <SideMenu logoutF={logoutFunction} />
         </div>
         <main className="dashboard-layout__page-view__body-content">
           <Outlet />
