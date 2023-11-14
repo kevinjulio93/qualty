40b3fce58923fe2a3e5fd031dd57aea81a41559c
Keiner Pajaro <keiner.pajaro@qrvey.com>
2023-11-14 18:12:57 -0500

    fix request url

--- src/models/request.model.ts
+++ src/models/request.model.ts
@@ -5,8 +5,8 @@ export class FecthRequestModel {
     
     constructor() {
         //this.url = import.meta.env.VITE_NODE_ENV === 'production'? import.meta.env.VITE_PROD_API_URL:import.meta.env.VITE_DEV_API_URL;
-        // this.url = "https://upedcucuta.com/api";
-        this.url = "http://localhost:3000";
+        this.url = "https://upedcucuta.com/api";
+        //this.url = "http://localhost:3000";
     }
 
 
